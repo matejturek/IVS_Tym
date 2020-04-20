2745b609b72e14a99ff04fd99dd54826acb8f1b6
David Sladký <davsladk@gmail.com>
2020-04-20 12:21:52 +0200

    Created comments for Doxyfile in test project.

--- src/Calculator/test/tst_math_lib_test.cpp
+++ src/Calculator/test/tst_math_lib_test.cpp
@@ -1,12 +1,16 @@
 /**
   * @file tst_math_lib_test.cpp
   * @author David Sladký
-  * @brief Test class for math_lib.
+  * @brief Tests for math_lib.
   */
 
 #include <QtTest>
 #include "math_lib.h"
 
+/**
+ * @class math_lib_test
+ * @brief The math_lib_test class
+ */
 class math_lib_test : public QObject
 {
     Q_OBJECT
@@ -70,210 +74,349 @@ math_lib_test::~math_lib_test()
 
 }
 
+/**
+ * @test 5 + 4
+ * @brief A math_lib.addtion() test
+ */
 void math_lib_test::addition1()
 {
     math_lib tmp;
     QVERIFY(tmp.addition(5, 4) == 9.0);
 }
 
+/**
+ * @brief 0 + 0
+ * @brief A math_lib.addtion() test
+ */
 void math_lib_test::addition2()
 {
     math_lib tmp;
     QVERIFY(tmp.addition(0, 0) == 0.0);
 }
 
+/**
+ * @test -99 999 + 2
+ * @brief A math_lib.addtion() test
+ */
 void math_lib_test::addition3()
 {
     math_lib tmp;
     QCOMPARE(tmp.addition(-99999, 2), -99997.0);
 }
 
+/**
+ * @test -7 + -42
+ * @brief A math_lib.addtion() test
+ */
 void math_lib_test::addition4()
 {
     math_lib tmp;
     QCOMPARE(tmp.addition(-7, -42), -49.0);
 }
 
+/**
+ * @test 66 + 100
+ * @brief A math_lib.addtion() test
+ */
 void math_lib_test::addition5()
 {
     math_lib tmp;
     QCOMPARE(tmp.addition(66, 100), 166.0);
 }
 
+/**
+ * @test 5 - 4
+ * @brief A math_lib.subtraction() test
+ */
 void math_lib_test::subtraction1()
 {
     math_lib tmp;
     QCOMPARE(tmp.subtraction(5, 4), 1.0);
 }
 
+/**
+ * @test 0 - 0
+ * @brief A math_lib.subtraction() test
+ */
 void math_lib_test::subtraction2()
 {
     math_lib tmp;
     QCOMPARE(tmp.subtraction(0, 0), 0.0);
 }
 
+/**
+ * @test -99 999 - 2
+ * @brief A math_lib.subtraction() test
+ */
 void math_lib_test::subtraction3()
 {
     math_lib tmp;
     QCOMPARE(tmp.subtraction(-99999, 2), -100001.0);
 }
 
+/**
+ * @test -7 - -42
+ * @brief A math_lib.subtraction() test
+ */
 void math_lib_test::subtraction4()
 {
     math_lib tmp;
     QCOMPARE(tmp.subtraction(-7, -42), 35.0);
 }
 
+/**
+ * @test 66 - 100
+ * @brief A math_lib.subtraction() test
+ */
 void math_lib_test::subtraction5()
 {
     math_lib tmp;
     QCOMPARE(tmp.subtraction(66, 100), -34.0);
 }
 
+/**
+ * @test 5 / 4
+ * @brief A math_lib.division() test
+ */
 void math_lib_test::division1()
 {
     math_lib tmp;
     QCOMPARE(tmp.division(5, 4), 5.0/4);
 }
 
+/**
+ * @test 0 / 0
+ * @brief A math_lib.division() test
+ */
 void math_lib_test::division2()
 {
     math_lib tmp;
     QVERIFY_EXCEPTION_THROWN(tmp.division(0, 0), std::logic_error);
 }
 
+/**
+ * @test -99 999 / 2
+ * @brief A math_lib.division() test
+ */
 void math_lib_test::division3()
 {
     math_lib tmp;
     QCOMPARE(tmp.division(-99999, 2), -99999.0/2);
 }
 
+/**
+ * @test -7 / -42
+ * @brief A math_lib.division() test
+ */
 void math_lib_test::division4()
 {
     math_lib tmp;
     QCOMPARE(tmp.division(-7, -42), -7.0/-42);
 }
 
+/**
+ * @test 66 / 100
+ * @brief A math_lib.division() test
+ */
 void math_lib_test::division5()
 {
     math_lib tmp;
     QCOMPARE(tmp.division(66, 100), 66.0/100);
 }
 
+/**
+ * @test 5 * 4
+ * @brief A math_lib.multiplication() test
+ */
 void math_lib_test::multiplication1()
 {
     math_lib tmp;
     QCOMPARE(tmp.multiplication(5, 4), 20.0);
 }
 
+/**
+ * @test 42 * 0
+ * @brief A math_lib.multiplication() test
+ */
 void math_lib_test::multiplication2()
 {
     math_lib tmp;
     QCOMPARE(tmp.multiplication(42, 0), 0.0);
 }
 
+/**
+ * @test -99 999 * 2
+ * @brief A math_lib.multiplication() test
+ */
 void math_lib_test::multiplication3()
 {
     math_lib tmp;
     QCOMPARE(tmp.multiplication(-99999, 2), -199998.0);
 }
 
+/**
+ * @brief A math_lib.multiplication() test
+ */
 void math_lib_test::multiplication4()
 {
     math_lib tmp;
     QCOMPARE(tmp.multiplication(-7, -42), 294.0);
 }
 
+/**
+ * @test 66 * 100
+ * @brief A math_lib.multiplication() test
+ */
 void math_lib_test::multiplication5()
 {
     math_lib tmp;
     QCOMPARE(tmp.multiplication(66, 100), 6600.0);
 }
 
+/**
+ * @test -5!
+ * @brief A math_lib.factorial() test
+ */
 void math_lib_test::factorial1()
 {
     math_lib tmp;
     QCOMPARE(tmp.factorial(-5), 0.0);
 }
 
+/**
+ * @test 1!
+ * @brief A math_lib.factorial() test
+ */
 void math_lib_test::factorial2()
 {
     math_lib tmp;
     QCOMPARE(tmp.factorial(1), 1.0);
 }
 
+/**
+ * @test 161!
+ * @brief A math_lib.factorial() test
+ */
 void math_lib_test::factorial3()
 {
     math_lib tmp;
     QVERIFY_EXCEPTION_THROWN(tmp.factorial(161), std::logic_error);
 }
 
+/**
+ * @test 10!
+ * @brief A math_lib.factorial() test
+ */
 void math_lib_test::factorial4()
 {
     math_lib tmp;
     QCOMPARE(tmp.factorial(10), 3628800.0);
 }
 
+/**
+ * @test 5!
+ * @brief A math_lib.factorial() test
+ */
 void math_lib_test::factorial5()
 {
     math_lib tmp;
     QCOMPARE(tmp.factorial(5), 120.0);
 }
 
+/**
+ * @test -5 ^ 0
+ * @brief A math_lib.power() test
+ */
 void math_lib_test::power1()
 {
     math_lib tmp;
     QCOMPARE(tmp.power(-5, 0), 1.0);
 }
 
+/**
+ * @test 1 ^ 1
+ * @brief A math_lib.power() test
+ */
 void math_lib_test::power2()
 {
     math_lib tmp;
     QCOMPARE(tmp.power(1, 1), 1.0);
 }
 
+/**
+ * @test 3 ^ -5
+ * @brief A math_lib.power() test
+ */
 void math_lib_test::power3()
 {
     math_lib tmp;
     QCOMPARE(tmp.power(3, -5), 0.0);
 }
 
+/**
+ * @test 10 ^ 10
+ * @brief A math_lib.power() test
+ */
 void math_lib_test::power4()
 {
     math_lib tmp;
     QCOMPARE(tmp.power(10, 10), 10000000000.0);
 }
 
+/**
+ * @test 5 ^ 2
+ * @brief A math_lib.power() test
+ */
 void math_lib_test::power5()
 {
     math_lib tmp;
     QCOMPARE(tmp.power(5,2), 25.0);
 }
 
+/**
+ * @test -5 ^ 1/-2
+ * @brief A math_lib.root() test
+ */
 void math_lib_test::root1()
 {
     math_lib tmp;
     QCOMPARE(tmp.root(-5, -2), 0.0);
 }
 
+/**
+ * @test 1 ^ 1/1
+ * @brief A math_lib.root() test
+ */
 void math_lib_test::root2()
 {
     math_lib tmp;
     QCOMPARE(tmp.root(1, 1), 1.0);
 }
 
+/**
+ * @test 3 ^ 1/2
+ * @brief A math_lib.root() test
+ */
 void math_lib_test::root3()
 {
     math_lib tmp;
     QCOMPARE(tmp.root(3, 2), std::pow(3.0, 1.0/2.0));
 }
 
+/**
+ * @test 10 ^ 1/10
+ * @brief A math_lib.root() test
+ */
 void math_lib_test::root4()
 {
     math_lib tmp;
     QCOMPARE(tmp.root(10, 10), std::pow(10.0, 1.0/10.0));
 }
 
+/**
+ * @test 27 ^ 1/3
+ * @brief A math_lib.root() test
+ */
 void math_lib_test::root5()
 {
     math_lib tmp;
